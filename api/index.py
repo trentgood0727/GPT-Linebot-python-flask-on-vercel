c3decd73b76ab271676b48d12b8d957daed17d09
chuan <chuan.chen@heroic-faith.com>
2023-04-25 10:00:29 +0800

    debug service crash on vercel

--- api/index.py
+++ api/index.py
@@ -2,8 +2,8 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
-from api.chatgpt import ChatGPT
-#from chatgpt import ChatGPT
+#from api.chatgpt import ChatGPT
+from chatgpt import ChatGPT
 
 import os
 import time
